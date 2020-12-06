61cbd5b0501e7f621f44a4c61cc9c7a27c8f2658
MarvTheRobot <marv@marvtherobot.com>
2020-12-06 20:55:24 +0000

    Refactor how commands/messages are hanled
    - Move commands to JSON
    
    - Add regEx for messages

---
+++ Start-Bot.ps1
@@ -0,0 +1,108 @@
+function Start-Bot {
+	[cmdletbinding()]
+	param(
+		$Server = 'irc.chat.twitch.tv',
+		$Port = 6697,
+		$UserName = 'marvro_bot',
+		$Channel = 'marvrobot'
+	)
+
+	# This needs fixing as it's trash and ugly
+	$AuthToken = Get-Content -Path $PSCommandPath.Replace('Start-Bot.ps1', 'token.txt')
+
+	Write-Verbose "Connecting stream"
+	$client = New-Object -TypeName System.Net.Sockets.TcpClient
+	$client.NoDelay = $true
+	$client.SendBufferSize = 81920
+	$client.ReceiveBufferSize = 81920
+	$client.Connect($server, $port)
+
+	$clientStream = $client.GetStream()
+	$sslStream    = New-Object -TypeName System.Net.Security.SslStream($clientStream, $false)
+	$sslStream.AuthenticateAsClient($server)
+
+	# Move to module level properties
+	$outputStream = New-Object -TypeName System.IO.StreamWriter($sslStream)
+	$outputStream.NewLine = "`r`n"
+	$inputStream  = New-Object -TypeName System.IO.StreamReader($sslStream)
+
+	Write-Verbose "Trying to log in"
+	if($authToken -isnot [object]){throw "No auth token"}
+	#Actually log in with username and authtoken
+	$outputStream.WriteLine("PASS $AuthToken")
+	$outputStream.WriteLine("NICK $UserName")
+	$outputStream.WriteLine("CAP REQ :twitch.tv/membership twitch.tv/tags twitch.tv/commands")
+	$outputStream.Flush()
+
+	Start-Sleep -Seconds 1
+	$outputStream.WriteLine("JOIN #$Channel")
+	$outputStream.Flush()
+
+	"Client Connected $($client.Connected)"
+	While ($client.Connected) {
+		While ($client.GetStream().DataAvailable -or $inputStream.Peek() -ne -1 ){
+			try {
+				$message = ConvertFrom-TwitchMessage( $inputStream.ReadLine() )
+				switch ($message.Command) {
+					'PING' {
+						Write-Output "You've been PING'd"
+						$response = 'PONG :tmi.twitch.tv'
+						$outputStream.WriteLine($response)
+						$outputStream.Flush()
+						Write-Output "You have PONG'd"
+					}
+					# Handle the message
+					'PRIVMSG' {
+						$response = Get-PrivateMessageResponse($message.Content)
+						$outputStream.WriteLine("PRIVMSG #$channel :'$response'")
+						$outputStream.Flush()
+					}
+					Default {	
+						$message
+					}
+				}
+			}
+			catch {
+				Throw "It broken: $_"	
+			}
+		}
+	}
+	$inputStream.Close()
+	$outputStream.Close()
+	$client.Close()
+}
+
+function ConvertFrom-TwitchMessage {
+	[cmdletbinding()]
+	param(
+		[string]$Message
+	)
+
+
+	$irctagregex = [Regex]::new('^(?:@([^ ]+) )?(?:[:]((?:(\w+)!)?\S+) )?(\S+)(?: (?!:)(.+?))?(?: [:](.+))?$')
+	$match = $irctagregex.Match($Message) #tags = 1
+	$props = @{
+		Prefix = $match.Groups[2].Value
+		User = $match.Groups[3].Value
+		Command = $match.Groups[4].Value
+		Params = $match.Groups[5].Value
+		Content = $match.Groups[6].Value
+	}
+	$returnObj = New-Object -TypeName PsObject -Property $props
+	Write-Output $returnObj
+}
+
+
+function Get-PrivateMessageResponse {
+	[cmdletbinding()]
+	param(
+		[string]$Message
+	)
+	if ($Message.StartsWith('!')) {
+		Write-Verbose "Command received"
+		$Message     = $Message.TrimStart('!')
+		$commandFile = $PSCommandPath.Replace('Start-Bot.ps1','Commands.json')
+		$commands    = Get-Content -Path $commandFile -Raw | ConvertFrom-Json -Depth 5
+		$commands.$Message
+	}
+}
--- TODO.md
+++ TODO.md
@@ -1,17 +1,26 @@
-1. TESTS MOTHA F***A
-1. Parse messages
-	 - Make it easier to see the command
-   - Emote support
-1. Have some commands
-   - In a datasource
-1. Be able to send private massages 
-1. Have some shoutouts
-1. Websockets - Maybe contribute back to https://github.com/potatoqualitee/tvbot
+- [ ] Bot shutdown
+- [ ] Bot re-connect (Has it been shutdown yet?):w
+- [ ] Bot registration... we are sending too many messages, too quickly.
+      We are simply too awesome for Twitch.... it cannot handle it... 
+      we need to let them know who we are and get BottyMcBotFace registered so 
+      we can send MOOOAAAARRR messages... we need to send all the messages.
+- [!] ConvertFrom-TwitchMessage 
+   ! -  Since converting we have lost emojis
+- [ ] Folder Structure
+    - Should be a module, wait until ti works
+- [ ] TESTS MOTHA F***A
+- Have some commands
+   - [ ] Shout outs, there must be a way to 'tag' people
+   - [x] Project (What are you working on?)
+   - [x] Desktop (What DE are you using?)
+- [x] Be able to send private massages 
+- Have some shoutouts
+- Websockets - Maybe contribute back to https://github.com/potatoqualitee/tvbot
+
+
+
 1. C#
 1. TypeScript/Node
 1. Python
 1. D
-
-
-
 1. Containers and K8s
